af49257d903e7ffff7fa7e3ac13b044c468437b1
jugeljke <jugeljke@gmail.com>
2021-02-17 17:20:17 +0400

    hh

---
+++ Firewall/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Firewall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayName = new System.Windows.Forms.TextBox();
+            this.displayNameLabel = new System.Windows.Forms.Label();
+            this.ipAddress = new System.Windows.Forms.TextBox();
+            this.port = new System.Windows.Forms.TextBox();
+            this.ipAddressLabel = new System.Windows.Forms.Label();
+            this.portLabel = new System.Windows.Forms.Label();
+            this.allowButton = new System.Windows.Forms.Button();
+            this.protocolLabel = new System.Windows.Forms.Label();
+            this.protocolListbox = new System.Windows.Forms.ListBox();
+            this.anyChecked = new System.Windows.Forms.CheckBox();
+            this.orLabel = new System.Windows.Forms.Label();
+            this.blockButton = new System.Windows.Forms.Button();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.boundListbox = new System.Windows.Forms.ListBox();
+            this.labelSelectInborOut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // displayName
+            // 
+            this.displayName.Location = new System.Drawing.Point(287, 120);
+            this.displayName.Name = "displayName";
+            this.displayName.Size = new System.Drawing.Size(209, 20);
+            this.displayName.TabIndex = 0;
+            // 
+            // displayNameLabel
+            // 
+            this.displayNameLabel.AutoSize = true;
+            this.displayNameLabel.Location = new System.Drawing.Point(209, 123);
+            this.displayNameLabel.Name = "displayNameLabel";
+            this.displayNameLabel.Size = new System.Drawing.Size(72, 13);
+            this.displayNameLabel.TabIndex = 1;
+            this.displayNameLabel.Text = "Display Name";
+            // 
+            // ipAddress
+            // 
+            this.ipAddress.Location = new System.Drawing.Point(287, 159);
+            this.ipAddress.Name = "ipAddress";
+            this.ipAddress.Size = new System.Drawing.Size(209, 20);
+            this.ipAddress.TabIndex = 2;
+            // 
+            // port
+            // 
+            this.port.Location = new System.Drawing.Point(287, 192);
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(70, 20);
+            this.port.TabIndex = 3;
+            this.port.TextChanged += new System.EventHandler(this.port_TextChanged);
+            // 
+            // ipAddressLabel
+            // 
+            this.ipAddressLabel.AutoSize = true;
+            this.ipAddressLabel.Location = new System.Drawing.Point(211, 162);
+            this.ipAddressLabel.Name = "ipAddressLabel";
+            this.ipAddressLabel.Size = new System.Drawing.Size(58, 13);
+            this.ipAddressLabel.TabIndex = 4;
+            this.ipAddressLabel.Text = "IP Address";
+            // 
+            // portLabel
+            // 
+            this.portLabel.AutoSize = true;
+            this.portLabel.Location = new System.Drawing.Point(241, 194);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(26, 13);
+            this.portLabel.TabIndex = 5;
+            this.portLabel.Text = "Port";
+            // 
+            // allowButton
+            // 
+            this.allowButton.Location = new System.Drawing.Point(287, 292);
+            this.allowButton.Name = "allowButton";
+            this.allowButton.Size = new System.Drawing.Size(75, 23);
+            this.allowButton.TabIndex = 6;
+            this.allowButton.Text = "Allow";
+            this.allowButton.UseVisualStyleBackColor = true;
+            this.allowButton.Click += new System.EventHandler(this.allowButton_Click);
+            // 
+            // protocolLabel
+            // 
+            this.protocolLabel.AutoSize = true;
+            this.protocolLabel.Location = new System.Drawing.Point(235, 224);
+            this.protocolLabel.Name = "protocolLabel";
+            this.protocolLabel.Size = new System.Drawing.Size(46, 13);
+            this.protocolLabel.TabIndex = 8;
+            this.protocolLabel.Text = "Protocol";
+            // 
+            // protocolListbox
+            // 
+            this.protocolListbox.FormattingEnabled = true;
+            this.protocolListbox.Items.AddRange(new object[] {
+            "TCP",
+            "UDP",
+            "Any"});
+            this.protocolListbox.Location = new System.Drawing.Point(304, 224);
+            this.protocolListbox.Name = "protocolListbox";
+            this.protocolListbox.Size = new System.Drawing.Size(120, 43);
+            this.protocolListbox.TabIndex = 9;
+            // 
+            // anyChecked
+            // 
+            this.anyChecked.AutoSize = true;
+            this.anyChecked.Checked = true;
+            this.anyChecked.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.anyChecked.Location = new System.Drawing.Point(404, 194);
+            this.anyChecked.Name = "anyChecked";
+            this.anyChecked.Size = new System.Drawing.Size(44, 17);
+            this.anyChecked.TabIndex = 10;
+            this.anyChecked.Text = "Any";
+            this.anyChecked.UseVisualStyleBackColor = true;
+            this.anyChecked.CheckedChanged += new System.EventHandler(this.anyChecked_CheckedChanged);
+            // 
+            // orLabel
+            // 
+            this.orLabel.AutoSize = true;
+            this.orLabel.Location = new System.Drawing.Point(372, 195);
+            this.orLabel.Name = "orLabel";
+            this.orLabel.Size = new System.Drawing.Size(16, 13);
+            this.orLabel.TabIndex = 11;
+            this.orLabel.Text = "or";
+            // 
+            // blockButton
+            // 
+            this.blockButton.Location = new System.Drawing.Point(386, 292);
+            this.blockButton.Name = "blockButton";
+            this.blockButton.Size = new System.Drawing.Size(75, 23);
+            this.blockButton.TabIndex = 12;
+            this.blockButton.Text = "Block";
+            this.blockButton.UseVisualStyleBackColor = true;
+            this.blockButton.Click += new System.EventHandler(this.blockButton_Click);
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.BackColor = System.Drawing.Color.Red;
+            this.errorLabel.Location = new System.Drawing.Point(12, 348);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(0, 13);
+            this.errorLabel.TabIndex = 13;
+            this.errorLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.errorLabel.Visible = false;
+            // 
+            // boundListbox
+            // 
+            this.boundListbox.FormattingEnabled = true;
+            this.boundListbox.Items.AddRange(new object[] {
+            "Inbound",
+            "Outbound"});
+            this.boundListbox.Location = new System.Drawing.Point(304, 84);
+            this.boundListbox.Name = "boundListbox";
+            this.boundListbox.Size = new System.Drawing.Size(65, 30);
+            this.boundListbox.TabIndex = 14;
+            // 
+            // labelSelectInborOut
+            // 
+            this.labelSelectInborOut.AutoSize = true;
+            this.labelSelectInborOut.Location = new System.Drawing.Point(211, 91);
+            this.labelSelectInborOut.Name = "labelSelectInborOut";
+            this.labelSelectInborOut.Size = new System.Drawing.Size(58, 13);
+            this.labelSelectInborOut.TabIndex = 15;
+            this.labelSelectInborOut.Text = "Select one";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelSelectInborOut);
+            this.Controls.Add(this.boundListbox);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.blockButton);
+            this.Controls.Add(this.orLabel);
+            this.Controls.Add(this.anyChecked);
+            this.Controls.Add(this.protocolListbox);
+            this.Controls.Add(this.protocolLabel);
+            this.Controls.Add(this.allowButton);
+            this.Controls.Add(this.portLabel);
+            this.Controls.Add(this.ipAddressLabel);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.ipAddress);
+            this.Controls.Add(this.displayNameLabel);
+            this.Controls.Add(this.displayName);
+            this.Name = "Form1";
+            this.Text = "Firewall Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox displayName;
+        private System.Windows.Forms.Label displayNameLabel;
+        private System.Windows.Forms.TextBox ipAddress;
+        private System.Windows.Forms.TextBox port;
+        private System.Windows.Forms.Label ipAddressLabel;
+        private System.Windows.Forms.Label portLabel;
+        private System.Windows.Forms.Button allowButton;
+        private System.Windows.Forms.Label protocolLabel;
+        private System.Windows.Forms.ListBox protocolListbox;
+        private System.Windows.Forms.CheckBox anyChecked;
+        private System.Windows.Forms.Label orLabel;
+        private System.Windows.Forms.Button blockButton;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.ListBox boundListbox;
+        private System.Windows.Forms.Label labelSelectInborOut;
+    }
+}
+
---
+++ Firewall/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetFwTypeLib;
+
+namespace Firewall
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            protocolListbox.SelectedIndex = 2;
+
+            //first path
+
+            //INetFwRule firewallRule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
+            //firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
+            //firewallRule.Description = "Used to block all internet access.";
+            //firewallRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
+            //firewallRule.Enabled = true;
+            //firewallRule.InterfaceTypes = "All";
+            //firewallRule.Name = "Block Internet";
+
+            //INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(
+            //    Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            //firewallPolicy.Rules.Add(firewallRule);
+
+
+        }
+
+        //SECOND PATH
+
+        //disable IP
+        private void DisableIpAddress(string port, string displayName, string ipAddress, string protocol, string inborout)
+        {
+            //var psCommand = $"New-NetFirewallRule -DisplayName \"<rule description>\" -Direction Inbound -LocalPort {port} -Protocol TCP -Action Allow";
+            var powershell = PowerShell.Create();
+
+            var blockCommand = $"New-NetFirewallRule -DisplayName {displayName} -Direction {inborout} -LocalPort {port} -Protocol {protocol} -Action Block -RemoteAddress {ipAddress}";
+
+            powershell.Commands.AddScript(blockCommand);
+
+            powershell.Invoke();
+
+            var ifAnyError = powershell.Streams.Error;
+
+            errorLabel.Visible = true;
+            try
+            {
+                errorLabel.Text = ifAnyError[0].ToString();
+            }
+            catch(Exception ex)
+            {
+                var msg = ex.Message;
+                errorLabel.BackColor = Color.Green;
+                errorLabel.Text = "Operation was successful";
+            }
+            
+        }
+
+        //enable IP
+        private void AllowIpAddress(string port,string displayName, string ipAddress, string protocol,string inborout)
+        {
+            var powershell = PowerShell.Create();
+
+            var allowCommand = $"New-NetFirewallRule -DisplayName {displayName} -Direction {inborout} -LocalPort {port} -Protocol {protocol} -Action Allow -RemoteAddress {ipAddress}";
+
+            powershell.Commands.AddScript(allowCommand);
+            var result = powershell.Invoke();
+            var ifAnyError = powershell.Streams.Error;
+
+            errorLabel.Visible = true;
+            try
+            {
+                errorLabel.Text = ifAnyError[0].ToString();
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+                errorLabel.BackColor = Color.Green;
+                errorLabel.Text = "Operation was successful";
+            }
+        }
+
+        private void allowButton_Click(object sender, EventArgs e)
+        {
+            if (anyChecked.Checked)
+            {
+                port.Text = "Any";
+            }
+            AllowIpAddress(port.Text, displayName.Text, ipAddress.Text, protocolListbox.SelectedItem.ToString(),boundListbox.SelectedItem.ToString());
+        }
+
+        private void anyChecked_CheckedChanged(object sender, EventArgs e)
+        {
+            port.Text = "Any";
+        }
+
+        private void port_TextChanged(object sender, EventArgs e)
+        {
+            anyChecked.Checked = false;
+        }
+
+        private void blockButton_Click(object sender, EventArgs e)
+        {
+            if (anyChecked.Checked)
+            {
+                port.Text = "Any";
+            }
+            DisableIpAddress(port.Text, displayName.Text, ipAddress.Text, protocolListbox.SelectedItem.ToString(),boundListbox.SelectedItem.ToString());
+        }
+    }
+}
